5764ff87a1d01a8dfa610bf1e1da998ad198c453
poppindouble <poppindouble@gmail.com>
2020-10-20 02:24:51 -0700

    Memory barrier at an act.

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,74 @@
 use std::cell::UnsafeCell;
-use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::{Arc, Barrier};
 use std::thread;
 
-struct UsizePair {
-    atom: AtomicUsize,
-    norm: UnsafeCell<usize>,
+struct SharedMemory {
+    pub num: UnsafeCell<usize>,
 }
 
-// UnsafeCell is not thread-safe. So manually mark our UsizePair to be Sync.
-// (Effectively telling the compiler "I'll take care of it!")
-unsafe impl Sync for UsizePair {}
-
-static NTHREADS: usize = 8;
-static NITERS: usize = 1000000;
+unsafe impl Sync for SharedMemory {}
 
 fn main() {
-    let upair = Arc::new(UsizePair::new(0));
+    let barrier = Arc::new(Barrier::new(3));
+    let shared_mem_x = Arc::new(SharedMemory{num: UnsafeCell::new(0)});
+    let shared_mem_y = Arc::new(SharedMemory{num: UnsafeCell::new(0)});
+    let shared_mem_r1 = Arc::new(SharedMemory{num: UnsafeCell::new(0)});
+    let shared_mem_r2 = Arc::new(SharedMemory{num: UnsafeCell::new(0)});
 
-    // Barrier is a counter-like synchronization structure (not to be confused
-    // with a memory barrier). It blocks on a `wait` call until a fixed number
-    // of `wait` calls are made from various threads (like waiting for all
-    // players to get to the starting line before firing the starter pistol).
-    let barrier = Arc::new(Barrier::new(NTHREADS + 1));
+    for i in 0..10000000 {
 
-    let mut children = vec![];
-
-    for _ in 0..NTHREADS {
-        let upair = upair.clone();
-        let barrier = barrier.clone();
-        children.push(thread::spawn(move || {
-            barrier.wait();
-
-            let mut v = 0;
-            while v < NITERS - 1 {
-                // Read both members `atom` and `norm`, and check whether `atom`
-                // contains a newer value than `norm`. See `UsizePair` impl for
-                // details.
-                let (atom, norm) = upair.get();
-                if atom != norm {
-                    // If `Acquire`-`Release` ordering is used in `get` and
-                    // `set`, then this statement will never be reached.
-                    println!("Reordered! {} != {}", atom, norm);
-                }
-                v = atom;
+        {
+            unsafe  {
+                *shared_mem_x.num.get() = 0;
+                *shared_mem_y.num.get() = 0;
+                *shared_mem_r1.num.get() = 0;
+                *shared_mem_r2.num.get() = 0;
             }
-        }));
-    }
-
-    barrier.wait();
-
-    for v in 1..NITERS {
-        // Update both members `atom` and `norm` to value `v`. See the impl for
-        // details.
-        upair.set(v);
-    }
-
-    for child in children {
-        let _ = child.join();
-    }
-}
-
-impl UsizePair {
-    pub fn new(v: usize) -> UsizePair {
-        UsizePair {
-            atom: AtomicUsize::new(v),
-            norm: UnsafeCell::new(v),
         }
-    }
 
-    pub fn get(&self) -> (usize, usize) {
-        let atom = self.atom.load(Ordering::Acquire); //Ordering::Acquire
+        let b1 = barrier.clone();
+        let smx_1 = shared_mem_x.clone();
+        let smy_1 = shared_mem_y.clone();
+        let smr1 = shared_mem_r1.clone();
+        let t1 = thread::spawn(move || {
+            b1.wait();
 
-        // If the above load operation is performed with `Acquire` ordering,
-        // then all writes before the corresponding `Release` store is
-        // guaranteed to be visible below.
+            unsafe {
+                *smx_1.num.get() = 1;
+            }
 
-        let norm = unsafe { *self.norm.get() };
-        (atom, norm)
-    }
+            unsafe {
+                *smr1.num.get() = *smy_1.num.get();
+            }
+        });
 
-    pub fn set(&self, v: usize) {
-        unsafe { *self.norm.get() = v };
+        let b2 = barrier.clone();
+        let smx_2 = shared_mem_x.clone();
+        let smy_2 = shared_mem_y.clone();
+        let smr2 = shared_mem_r2.clone();
+        let t2 = thread::spawn(move || {
+            b2.wait();
 
-        // If the below store operation is performed with `Release` ordering,
-        // then the write to `norm` above is guaranteed to be visible to all
-        // threads that "loads `atom` with `Acquire` ordering and sees the same
-        // value that was stored below". However, no guarantees are provided as
-        // to when other readers will witness the below store, and consequently
-        // the above write. On the other hand, there is also no guarantee that
-        // these two values will be in sync for readers. Even if another thread
-        // sees the same value that was stored below, it may actually see a
-        // "later" value in `norm` than what was written above. That is, there
-        // is no restriction on visibility into the future.
+            unsafe {
+                *smy_2.num.get() = 1;
+            }
 
-        self.atom.store(v, Ordering::Release); //Ordering::Release
+            unsafe {
+                *smr2.num.get() = *smx_2.num.get();
+            }
+        });
+
+        barrier.wait();
+        t1.join().unwrap();
+        t2.join().unwrap();
+
+        let r1 = shared_mem_r1.num.get();
+        let r2 = shared_mem_r2.num.get();
+
+        unsafe {
+            if *r1 == *r2 && *r1 == 0 {
+                println!("got it! r1: {:?}, r2: {:?}, current iteration number: {:?}", *r1, *r2, i);
+            }
+        }
     }
 }
